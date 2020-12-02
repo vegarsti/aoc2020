d53e1522b9da19debcfddbcc524efc6b8b033c9d
Vegard Stikbakke <vegard.stikbakke@gmail.com>
2020-12-02 08:06:17 +0100

    Not sure why I used enumerate

--- 01_1.py
+++ 01_1.py
@@ -1,8 +1,8 @@
 with open("input/1.txt", "r") as f:
     numbers = [int(line) for line in f.readlines()]
 
-for _, number1 in enumerate(numbers):
-    for _, number2 in enumerate(numbers):
+for number1 in numbers:
+    for number2 in numbers:
         if number1 + number2 == 2020:
             print(number1 * number2)
             exit(0)
--- 01_2.py
+++ 01_2.py
@@ -1,9 +1,9 @@
 with open("input/1.txt", "r") as f:
     numbers = [int(line) for line in f.readlines()]
 
-for _, number1 in enumerate(numbers):
-    for _, number2 in enumerate(numbers):
-        for _, number3 in enumerate(numbers):
+for number1 in numbers:
+    for number2 in numbers:
+        for number3 in numbers:
             if number1 + number2 + number3 == 2020:
                 print(number1 * number2 * number3)
                 exit(0)
