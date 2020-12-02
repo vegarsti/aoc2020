1e96ee2ad31009e3505a1bd409100f786d38502e
Vegard Stikbakke <vegard.stikbakke@gmail.com>
2020-12-02 08:41:38 +0100

    Minor

--- 02_1.py
+++ 02_1.py
@@ -5,9 +5,9 @@ with open("input/02.txt", "r") as f:
 def valid_line(line: str) -> bool:
     minmax, letter_with_colon, password = line.split()
     letter = letter_with_colon[0]
-    min_, max_ = minmax.split("-")
+    min_, max_ = (int(i) for i in minmax.split("-"))
     counts = password.count(letter)
-    return int(min_) <= counts <= int(max_)
+    return min_ <= counts <= max_
 
 
 print(sum(valid_line(line) for line in lines))
