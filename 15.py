c7682d3e783c289b38b0d9827c1ec7aab5a67ce2
Vegard Stikbakke <vegard.stikbakke@gmail.com>
2020-12-15 23:09:33 +0100

    Split day 15

--- 15.py
+++
@@ -1,8 +0,0 @@
-starting_numbers = [1, 2, 16, 19, 18, 0]
-previously = {n: i + 1 for i, n in enumerate(starting_numbers[:-1])}
-spoken = starting_numbers[-1]
-turn = len(starting_numbers) - 1
-for limit in (2020, 30000000):
-    while (turn := turn + 1) < limit:
-        previously[spoken], spoken = turn, turn - previously.get(spoken, turn)
-    print(spoken)
---
+++ 15_1.py
@@ -0,0 +1,6 @@
+*starting_numbers, spoken = [1, 2, 16, 19, 18, 0]
+previously = {n: i + 1 for i, n in enumerate(starting_numbers)}
+turn = len(starting_numbers)
+while (turn := turn + 1) < 2020:
+    previously[spoken], spoken = turn, turn - previously.get(spoken, turn)
+print(spoken)
