52aa058fed26bf467e41b3b2e7f8ac00c50a99e5
Vegard Stikbakke <vegard.stikbakke@gmail.com>
2020-12-15 21:15:00 +0100

    Day 15 faster

--- 15.py
+++ 15.py
@@ -1,14 +1,8 @@
-from collections import defaultdict
-
 starting_numbers = [1, 2, 16, 19, 18, 0]
-history = defaultdict(list)
-for i, n in enumerate(starting_numbers):
-    history[n].append(i)
+previously = {n: i + 1 for i, n in enumerate(starting_numbers[:-1])}
 spoken = starting_numbers[-1]
 turn = len(starting_numbers) - 1
 for limit in (2020, 30000000):
     while (turn := turn + 1) < limit:
-        spoken = history[spoken][-1] - history[spoken][-2] if len(history[spoken]) > 1 else 0
-        history[spoken].append(turn)
+        previously[spoken], spoken = turn, turn - previously.get(spoken, turn)
     print(spoken)
-    turn -= 1
